47733e62a6c94c825bea820fc224db6f46d21a5b
garzajpx <jupagaru@gmail.com>
2025-09-25 16:52:19 -0500

    Se agregan señales

--- 01-bases/src/app/pages/counter/counter.ts
+++ 01-bases/src/app/pages/counter/counter.ts
@@ -1,23 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   selector: 'app-counter-page',
   templateUrl: './counter.html',
+  styles: `
+    button {
+      padding: 5px;
+      margin: 5px 10px;
+      width: 75px;
+  `
 })
+
 export class CounterPageComponent {
 
+
   counter = 15;
+  counterSignal = signal(15);
 
   increaseBy(value: number){
     this.counter += value;
+    this.counterSignal.update( current => current + value );
+
   }
 
   decreaseBy(value: number){
     this.counter -= value;
+    this.counterSignal.update( current => current - value );
   }
 
   resetCounter(){
-    this.counter = 15;
+    this.counter = 0;
+    this.counterSignal.set(0);
+
   }
 
 }
